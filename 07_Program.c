59ee483c4cb927b5ac78b7f94fae530484d6bb2d
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-08-09 16:18:06 +0530

    Third Commit

---
+++ 07_Program.c
@@ -0,0 +1,9 @@
+//Introduction to while loop
+#include<stdio.h>
+void main(){
+	int a=10;
+	while(a<20){
+		printf("a value is %d\n",a);
+		a++;
+	}
+}
---
+++ 08_Program.c
@@ -0,0 +1,10 @@
+//Introduction to Do While Loop
+#include<stdio.h>
+int main(){
+	int a=0;
+	do{
+		printf("a value is %d\n",a);
+		a++;
+	}
+	while(a<20);
+}
