15e8f7f6c05b8dae8f3c0d8b89a117f888271a33
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-08-09 11:48:29 +0530

    Second Commit

---
+++ 02_ProgramV2.c
@@ -0,0 +1,15 @@
+//Write a program to print Fahrenheit to Celsius Table.
+#include<stdio.h>
+int main(){
+float fahr,celsius;
+int lower,upper,step;
+lower=0; //Lower limit of temperature table
+upper=300; //Upper limit of temperature table
+step=20;
+fahr=lower;
+while(fahr<=upper){
+    celsius=5.0*(fahr-32.0)/9.0;
+    printf("%3.0f\t%6.3f\n",fahr,celsius);
+    fahr=fahr+step;
+}
+}
---
+++ 05_Program.c
@@ -0,0 +1,24 @@
+//Program to display weekdays according to the given number
+#include<stdio.h>
+void main(){
+	int weekDay;
+	printf("Enter WeekDay in Numbers:");
+	scanf("%d",&weekDay);
+	switch(weekDay){
+		case 1:printf("Monday");
+		break;
+		case 2:printf("Tuesday");
+		break;
+		case 3:printf("Wednesday");
+		break;
+		case 4:printf("Thursday");
+		break;
+		case 5:printf("Friday");
+		break;
+		case 6:printf("Saturday");
+		break;
+		case 7:printf("Sunday");
+		break;
+		default: printf("ERROR - Enter No. between 1 to 7");
+	}
+}
